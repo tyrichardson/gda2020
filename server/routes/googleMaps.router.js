daf777f499d43824b80e49f6084ef52cdfd6ec9b
Ty Richardson <ty.richardson@gmail.com>
2020-08-10 17:12:00 -0500

    updated staticMapIcon with encoded URL

--- server/routes/googleMaps.router.js
+++ server/routes/googleMaps.router.js
@@ -23,8 +23,9 @@ router.get('/', (req, res) => {
     }
 });
 
+const staticMapIcon = `https://lh3.googleusercontent.com/-uJSgG2XuCgOzzlU3n5P8lYeRfIcv41nx-3lMFYqS53m4ITg3s27wGFkLgb7FwawMQb-rPQH_f4PIfG4xjBwv8ZU19ElhYGcOVlZtyGzkQ0feJfp81eyzXMiygoXWAZDs_zub_CDazw6UaMhrUYpGyrEEKQ_8LW39Cz9mXM_GfO7EdVfDsuq76lVipBGrOIgORlfdROaTJu4mmzCH2okmAptWhJ5SxkfNvhbEcqHPqQzgbM-40Jm4W7RfCmwDnp3nKWAc1335t1CqKA402gbV6nrjBmvtf4SQOVW0A0HFvPpFjYFizx3v4xRGfqZBJdAQy5OU3MdoEyfIXDNV3AOSxG-RozmzEwSt4InkgVKYNgy4JodIZ_LOsVvWwVEecN9Topf47AwNAmvuwm5G_fL4sx__oGkk6Bypa0PxFu9La17PlqBHiHi3iJsKxUwIVZhMl_7ucQvKnw3pobbZFiI4tJILLbBMBlXNnyIKxMRe5t-f4LyvBNFa_FNvuJx1sbogWUk-eJoJfqdci8SGv2Oid7MS3PRy0Gxo4qI9jrWDaYT7KGdN5QeF1YaF2QTSHQkar65YP3ivziddgF9g7Uy0H0G0V9DZZnnxcuADkPKmkCQ8Z9Lp_R40MCmTX0P7lW5tOFp0djMfTV8u_2HXszUzfYbbXCNOTf2iqTXG9JyKtlhVMFoki2W0Ef8o8U0LA=s64-no?authuser=0`
+
 router.get('/latLong', (req, res) => {
-  //  if(req.isAuthenticated()){
         console.log('latLong route ', req.query.name);
         let queryText = 'SELECT "lat", "lng" FROM "story" WHERE "id" = $1;';
         pool.query(queryText, [req.query.name])
@@ -32,14 +33,13 @@ router.get('/latLong', (req, res) => {
             let lat = parseFloat(result.rows[0].lat);
             let long = parseFloat(result.rows[0].lng);
             console.log('staticMap result.rows ', lat, long);
-            let urlStatic = `http://maps.googleapis.com/maps/api/staticmap?&size=300x200&zoom=10&style=visibility:on&style=feature:water|element:geometry|visibility:on&style=feature:landscape|element:geometry|visibility:on&markers=anchor:center|icon:https://bit.ly/30zAhoz|${lat},${long}&key=${googleMapsKey}`;
+            let urlStatic = `http://maps.googleapis.com/maps/api/staticmap?&size=300x200&zoom=10&style=visibility:on&style=feature:water|element:geometry|visibility:on&style=feature:landscape|element:geometry|visibility:on&markers=anchor:center|icon:${staticMapIcon}|${lat},${long}&key=${googleMapsKey}`;
             console.log('googleMapsRouter latLong result', urlStatic);
             res.send(urlStatic);
         })
         .catch((error) => {
            res.sendStatus(500);
         }); 
-  //  }
 });
 
 module.exports = router;
